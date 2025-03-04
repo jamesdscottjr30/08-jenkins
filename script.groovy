6a504e8b7db5752444f29bce1444c6c460d8e3eb
jamesdscottjr30 <124324024+jamesdscottjr30@users.noreply.github.com>
2025-03-04 15:20:43 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,13 +1,20 @@
 def buildApp() {
     echo 'building the application...'
+    sh 'maven package'
 }
 
-def testApp() {
-    echo 'testing the application...'
+def buildImage() {
+    echo 'building the docker image...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                        sh 'docker build -t j30brooks/demo-app:jma-2.0 .'
+                        sh 'echo $PASS | docker login -u $USER --password-stdin'
+                        sh 'docker push j30brooks/demo-app:jma-2.0'
     }
+}
 
 def deployApp() {
     echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
+    
 }
+
 return this
